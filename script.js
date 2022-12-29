9aa19e33e3f5348c9cdfa62bd7d6b42eafd6d0e8
Raptor7742 <106883599+Raptor7742@users.noreply.github.com>
2022-12-29 01:57:15 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,157 @@
+const playerInstance = jwplayer("player").setup({
+  controls: true,
+  sharing: true,
+  displaytitle: true,
+  displaydescription: true,
+  abouttext: "",
+  aboutlink: "",
+
+  skin: {
+    name: "netflix"
+  },
+
+  logo: {
+    file:
+      "",
+    link: ""
+  },
+
+  captions: {
+    color: "#FFF",
+    fontSize: 14,
+    backgroundOpacity: 0,
+    edgeStyle: "raised"
+  },
+
+  playlist: [
+    {
+      title: "Le Secret de la cité perdue",
+      description: "Vous regardez",
+      image: "https://www.geekgeneration.fr/wp-content/uploads/2022/02/Le-Secret-de-la-Cite%CC%81-Perdue.jpg",
+      sources: [
+        {
+          file:
+            "https://edef5.pcloud.com/cBZ0fEzJ8ZbR8XkBZhN04ZZn19Ec7ZQ5ZZlm5ZkZ2QECV7ZJ7ZMVZz0ZxVZc7ZHkZEVZT0Z4VZjXZNkZB7Z3VZ9XZB0kmZyyE6BegVsFuP4Btx8kaDRpWEhuuV/The.Lost.City.2022.TRUEFRENCH.1080p.WEB-DL.DD5.1.H.264.mp4",
+          label: "1080p",
+          default: true
+        },
+        {
+          file:
+            "https://edef5.pcloud.com/cBZ0fEzJ8ZbR8XkBZhN04ZZn19Ec7ZQ5ZZlm5ZkZ2QECV7ZJ7ZMVZz0ZxVZc7ZHkZEVZT0Z4VZjXZNkZB7Z3VZ9XZB0kmZyyE6BegVsFuP4Btx8kaDRpWEhuuV/The.Lost.City.2022.TRUEFRENCH.1080p.WEB-DL.DD5.1.H.264.mp4",
+          label: "720p"
+        },
+        {
+          file:
+            "https://edef5.pcloud.com/cBZ0fEzJ8ZbR8XkBZhN04ZZn19Ec7ZQ5ZZlm5ZkZ2QECV7ZJ7ZMVZz0ZxVZc7ZHkZEVZT0Z4VZjXZNkZB7Z3VZ9XZB0kmZyyE6BegVsFuP4Btx8kaDRpWEhuuV/The.Lost.City.2022.TRUEFRENCH.1080p.WEB-DL.DD5.1.H.264.mp4",
+          label: "480p"
+        },
+        {
+          file:
+            "https://edef5.pcloud.com/cBZ0fEzJ8ZbR8XkBZhN04ZZn19Ec7ZQ5ZZlm5ZkZ2QECV7ZJ7ZMVZz0ZxVZc7ZHkZEVZT0Z4VZjXZNkZB7Z3VZ9XZB0kmZyyE6BegVsFuP4Btx8kaDRpWEhuuV/The.Lost.City.2022.TRUEFRENCH.1080p.WEB-DL.DD5.1.H.264.mp4",
+          label: "360p"
+        },
+        {
+          file:
+            "https://edef5.pcloud.com/cBZ0fEzJ8ZbR8XkBZhN04ZZn19Ec7ZQ5ZZlm5ZkZ2QECV7ZJ7ZMVZz0ZxVZc7ZHkZEVZT0Z4VZjXZNkZB7Z3VZ9XZB0kmZyyE6BegVsFuP4Btx8kaDRpWEhuuV/The.Lost.City.2022.TRUEFRENCH.1080p.WEB-DL.DD5.1.H.264.mp4",
+          label: "240p"
+        },
+        {
+          file:
+            "https://upload.wikimedia.org/wikipedia/commons/transcoded/7/74/Sprite_Fright_-_Open_Movie_by_Blender_Studio.webm/Sprite_Fright_-_Open_Movie_by_Blender_Studio.webm.160p.webm",
+          label: "160p"
+        }
+      ],
+      
+      tracks: [
+        {
+          file: "https://cdn.jwplayer.com/strips/iYfADWO1-120.vtt",
+          kind: "thumbnails"
+        }
+      ]
+    }
+  ],
+  advertising: {
+    client: "vast",
+    schedule: [
+      {
+        offset: "pre",
+        tag:
+          "https://www.videosprofitnetwork.com/watch.xml?key=d904b92c1f6cc769c59d030320a66f69"
+      }
+    ]
+  }
+});
+
+playerInstance.on("ready", function () {
+  const buttonId = "download-video-button";
+  const iconPath =
+    "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgd2lkdGg9IjY0IiBoZWlnaHQ9IjY0Ij48cGF0aCBmaWxsPSJub25lIiBkPSJNMCAwaDI0djI0SDB6Ii8+PHBhdGggZD0iTTMgMTloMTh2Mkgzdi0yem0xMC01LjgyOEwxOS4wNzEgNy4xbDEuNDE0IDEuNDE0TDEyIDE3IDMuNTE1IDguNTE1IDQuOTI5IDcuMSAxMSAxMy4xN1YyaDJ2MTEuMTcyeiIgZmlsbD0icmdiYSgyNDcsMjQ3LDI0NywxKSIvPjwvc3ZnPg==";
+  const tooltipText = "Download Video";
+
+  // Call the player's `addButton` API method to add the custom button
+  playerInstance.addButton(iconPath, tooltipText, buttonClickAction, buttonId);
+
+  // This function is executed when the button is clicked
+  function buttonClickAction() {
+    const playlistItem = playerInstance.getPlaylistItem();
+    const anchor = document.createElement("a");
+    const fileUrl = playlistItem.file;
+    anchor.setAttribute("href", fileUrl);
+    const downloadName = playlistItem.file.split("/").pop();
+    anchor.setAttribute("download", downloadName);
+    anchor.style.display = "none";
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+  }
+
+  // Move the timeslider in-line with other controls
+  const playerContainer = playerInstance.getContainer();
+  const buttonContainer = playerContainer.querySelector(".jw-button-container");
+  const spacer = buttonContainer.querySelector(".jw-spacer");
+  const timeSlider = playerContainer.querySelector(".jw-slider-time");
+  buttonContainer.replaceChild(timeSlider, spacer);
+
+  // Detect adblock
+  playerInstance.on("adBlock", () => {
+    const modal = document.querySelector("div.modal");
+    modal.style.display = "flex";
+
+    document
+      .getElementById("close")
+      .addEventListener("click", () => location.reload());
+  });
+
+  // Forward 10 seconds
+  const rewindContainer = playerContainer.querySelector(
+    ".jw-display-icon-rewind"
+  );
+  const forwardContainer = rewindContainer.cloneNode(true);
+  const forwardDisplayButton = forwardContainer.querySelector(
+    ".jw-icon-rewind"
+  );
+  forwardDisplayButton.style.transform = "scaleX(-1)";
+  forwardDisplayButton.ariaLabel = "Forward 10 Seconds";
+  const nextContainer = playerContainer.querySelector(".jw-display-icon-next");
+  nextContainer.parentNode.insertBefore(forwardContainer, nextContainer);
+
+  // control bar icon
+  playerContainer.querySelector(".jw-display-icon-next").style.display = "none"; // hide next button
+  const rewindControlBarButton = buttonContainer.querySelector(
+    ".jw-icon-rewind"
+  );
+  const forwardControlBarButton = rewindControlBarButton.cloneNode(true);
+  forwardControlBarButton.style.transform = "scaleX(-1)";
+  forwardControlBarButton.ariaLabel = "Forward 10 Seconds";
+  rewindControlBarButton.parentNode.insertBefore(
+    forwardControlBarButton,
+    rewindControlBarButton.nextElementSibling
+  );
+
+  // add onclick handlers
+  [forwardDisplayButton, forwardControlBarButton].forEach((button) => {
+    button.onclick = () => {
+      playerInstance.seek(playerInstance.getPosition() + 10);
+    };
+  });
+});
